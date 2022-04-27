83f10701d1d7e93417e731a6a7eb38c3ff959e8b
Yitzchak Lewkowicz <yitzchaklewkow@yahoo.com>
2022-04-26 23:52:33 -0600

    commit tree 04104d1128f78bfeffe03acc720b363a58410661

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier){
+    return fare => multiplier * fare;
+}
+
+
+const fareDoubler = createFareMultiplier(2)
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(drivers, func){
+    return func(drivers);
+}
